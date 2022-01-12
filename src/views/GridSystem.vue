ca205e3c7198d4979938019213632f9324634f9d
DESKTOP-TPQHNMR\baek <tmdduq5498@gmail.com>
2022-01-12 17:15:42 +0900

    initial commit

---
+++ src/views/GridSystem.vue
@@ -0,0 +1,169 @@
+<template>
+  <v-container
+    fluid
+  >
+    <v-card class="pa-3">
+      <v-row dense>
+        <v-col>
+          <v-card
+            class="pa-3"
+            outline
+          >
+            Col
+          </v-card>
+        </v-col>
+        <v-col>
+          <v-card
+            class="pa-3"
+            outline
+          >
+            Col
+          </v-card>
+        </v-col>
+        <v-spacer />
+        <v-col>
+          <v-card
+            class="pa-3"
+            outline
+          >
+            Col
+          </v-card>
+        </v-col>
+      </v-row>
+      <v-row no-gutters>
+        <v-spacer />
+        <v-col>
+          <v-card
+            class="pa-3"
+            outline
+          >
+            Col
+          </v-card>
+        </v-col>
+        <v-col>
+          <v-card
+            class="pa-3"
+            outline
+          >
+            Col
+          </v-card>
+        </v-col>
+        <v-col>
+          <v-card
+            class="pa-3"
+            outline
+          >
+            Col
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-card>
+
+
+    <v-card class="mt-5 pa-3">
+      <v-row
+        :style="{height: '120px'}"
+        class="red"
+        align="center"
+      >
+        <v-col>
+          <v-card
+            class="pa-3"
+            outline
+          >
+            Col
+          </v-card>
+        </v-col>
+        <v-col>
+          <v-card
+            class="pa-3"
+            outline
+          >
+            Col
+          </v-card>
+        </v-col>
+        <v-col>
+          <v-card
+            class="pa-3"
+            outline
+          >
+            Col
+          </v-card>
+        </v-col>
+      </v-row>
+
+      <v-row
+        :style="{height: '120px'}"
+        class="yellow"
+      >
+        <v-col align-self="end">
+          <v-card
+            class="pa-3"
+            outline
+          >
+            Col
+          </v-card>
+        </v-col>
+        <v-col align-self="center">
+          <v-card
+            class="pa-3"
+            outline
+          >
+            Col
+          </v-card>
+        </v-col>
+        <v-col>
+          <v-card
+            class="pa-3"
+            outline
+          >
+            Col
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-card>
+
+    <v-card class="mt-5 pa-3">
+      <v-row justify="center">
+        <v-col
+          cols="auto"
+        >
+          <v-card
+            class="pa-3"
+            outline
+          >
+            Col
+          </v-card>
+        </v-col>
+        <v-col
+          cols="auto"
+        >
+          <v-card
+            class="pa-3"
+            outline
+          >
+            Col
+          </v-card>
+        </v-col>
+        <v-col
+          cols="auto"
+        >
+          <v-card
+            class="pa-3"
+            outline
+          >
+            Col
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-card>
+  </v-container>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style lang="">
+
+</style>
